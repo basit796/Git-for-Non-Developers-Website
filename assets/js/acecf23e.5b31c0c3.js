510ca920dab27e01625e359d98ced4e2cbf958f9
basit796 <basitkhalid796@gmail.com>
2025-12-19 21:53:59 +0500

    Deploy website - based on 5df284d9d7669d60de42388ba81ec12258467f9b

---
+++ assets/js/acecf23e.5b31c0c3.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkwebsite=globalThis.webpackChunkwebsite||[]).push([[1903],{1912(e){e.exports=JSON.parse('{"blogBasePath":"/Git-for-Non-Developers-Website/blog","blogTitle":"Blog","authorsListPath":"/Git-for-Non-Developers-Website/blog/authors"}')}}]);
